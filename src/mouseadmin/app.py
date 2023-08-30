408609bb0e7f96774349d662923cbd631fd7685d
well <well1912@gmail.com>
2023-08-30 08:30:54 -0700

    dev dependencies

--- src/mouseadmin/app.py
+++ src/mouseadmin/app.py
@@ -1,8 +1,12 @@
 # save this as app.py
 from flask import Flask
 
+from mouseadmin import neocities
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello, World!!!"
+    return "Hello, World..."
