a8568536788bf0f9a1881bee4407412fdd215b17
well <well1912@gmail.com>
2023-08-30 10:23:02 -0700

    sort reviews, domain object

--- src/mouseadmin/app.py
+++ src/mouseadmin/app.py
@@ -1,4 +1,6 @@
-# save this as app.py
+import re
+from dataclasses import dataclass
+from datetime import datetime
 import os
 from flask import Flask, render_template
 
@@ -8,12 +10,35 @@ from mouseadmin import neocities
 app = Flask(__name__)
 
 
+@dataclass
+class Review:
+    is_directory: bool
+    path: str
+    sha1_hash: str
+    size: int
+    updated_at: str
 
+    @classmethod
+    def get_reviews(cls, items) -> list["Review"]:
+        reviews = [
+            Review(**item)
+            for item in items
+            if "reviews/" in item["path"] and not item["is_directory"]
+        ]
+        return sorted(reviews, key=lambda review: review.date)
+
+    @property
+    def date(self):
+        [datestr] = re.match("reviews/([0-9]+-[0-9]+-[0-9]+)", self.path).groups()
+        return datetime.fromisoformat(datestr).date()
+
+
+def fetch_reviews(client):
+    items = client.listitems()["files"]
+    return Review.get_reviews(items)
 
 
 @app.route("/reviews")
 def reviews():
     client = neocities.NeoCities(api_key=os.getenv("MOUSEADMIN_SITE_API_KEY"))
-    items = client.listitems()["files"]
-    review_items = [item for item in items if "reviews/" in item['path']]
-    return render_template("review_list.html", items=review_items)
+    return render_template("review_list.html", items=fetch_reviews(client))
