c4a82429f0cbe8980566ef504f95b0b33ac6fde9
well <well1912@gmail.com>
2023-08-30 08:52:46 -0700

    review page list

--- src/mouseadmin/app.py
+++ src/mouseadmin/app.py
@@ -1,5 +1,6 @@
 # save this as app.py
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 from mouseadmin import neocities
 
@@ -7,6 +8,12 @@ from mouseadmin import neocities
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello, World..."
+
+
+
+@app.route("/reviews")
+def reviews():
+    client = neocities.NeoCities(api_key=os.getenv("MOUSEADMIN_SITE_API_KEY"))
+    items = client.listitems()["files"]
+    review_items = [item for item in items if "reviews/" in item['path']]
+    return render_template("review_list.html", items=review_items)
