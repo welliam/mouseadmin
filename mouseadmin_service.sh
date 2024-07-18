7b68f077cac77efbaaf677e362a3ec6f74550a4f
well <well1912@gmail.com>
2024-07-18 18:36:07 +0100

    fix

--- mouseadmin_service.sh
+++ mouseadmin_service.sh
@@ -2,6 +2,6 @@
 # sudo cp mouseadmin_service.sh /usr/bin/mouseadmin_service.sh
 # sudo chmod +x /usr/bin/mouseadmin_service.sh
 
-cd /home/wellpi/prog/mouseadmin
+cd /home/well/prog/mouseadmin
 . env/bin/activate
 FLASK_APP=src/mouseadmin/app.py flask run --host 0.0.0.0
